be2ef3369fbaab3fd4075ef5d7e1f50b05fbaf24
Diogo Bettencourt <diogobettencourt97@gmail.com>
2020-03-10 11:21:48 -0100

    Optimized Mobile Utility Classes and Added Properties Files for Easy Configuration

---
+++ AutomationFoundation/src/test/java/com/automation/foundation/utilityClasses/MobileStarter.java
@@ -0,0 +1,145 @@
+package com.automation.foundation.utilityClasses;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MobileStarter {
+
+    // Project-wide Properties File
+    private static Properties properties;
+
+    // Cucumber Methods
+    @Given("^I have started the Android Application$")
+    public void startAndroidApp1() {
+
+        // Testing Android
+        MobileUtility.setTestingAndroid(true);
+
+        // Set Android App Variant (Optional)
+        setAndroidAppVariant("");
+
+        // Starts the App
+        MobileUtility.startAndroidApp();
+    }
+
+    @Given("^I have started the iOS Application$")
+    public void startIOSApp1() {
+
+        // Testing iOS
+        MobileUtility.setTestingIOS(true);
+
+        // Set Android App Variant (Optional)
+        setIOSdAppVariant("");
+
+        // Starts the App
+        MobileUtility.startIOSApp();
+    }
+
+    @Then("^I should Close the Android Application$")
+    public void closeAndroidApp() {
+
+        // Testing Android
+        MobileUtility.setTestingAndroid(false);
+
+        // Reset App Variant
+        setAndroidAppVariant(null);
+
+        // Close Android App
+        MobileUtility.getAndroidDriver().closeApp();
+
+        // Completing Test
+        Assert.assertFalse(MobileUtility.isTestingAndroid());
+    }
+
+    @Then("^I should Close the iOS Application$")
+    public void closeIOSApp() {
+
+        // Testing iOS
+        MobileUtility.setTestingIOS(false);
+
+        // Reset App Variant
+        setIOSdAppVariant(null);
+
+        // Close iOS App
+        MobileUtility.getIosDriver().closeApp();
+
+        // Completing Test
+        Assert.assertFalse(MobileUtility.isTestingIOS());
+    }
+
+    @After
+    public void checkIfFailed(Scenario scenario) {
+
+        if (scenario.isFailed()) {
+            takeScreenshot();
+        }
+    }
+
+    // Failed Test Screenshot Method
+    private void takeScreenshot() {
+
+        File sourceFile;
+        File destinationFile;
+
+        // Takes Screenshot of Android Simulator & Saves
+        if (MobileUtility.isTestingAndroid()) {
+
+            try {
+                sourceFile = ((TakesScreenshot) MobileUtility.getAndroidDriver()).getScreenshotAs(OutputType.FILE);
+                destinationFile = new File(getValue("android.screenshots") + MobileUtility.randomNumber() + ".png");
+                FileUtils.copyFile(sourceFile, destinationFile);
+            } catch (IOException ex) {
+                System.out.println("Unable to Take Screenshot of Android Simulator");
+            }
+
+        }
+
+        // Takes Screenshot of iOS Simulator & Saves
+        if (MobileUtility.isTestingIOS()) {
+
+            try {
+                sourceFile = ((TakesScreenshot) MobileUtility.getIosDriver()).getScreenshotAs(OutputType.FILE);
+                destinationFile = new File(getValue("ios.screenshots") + MobileUtility.randomNumber() + ".png");
+                FileUtils.copyFile(sourceFile, destinationFile);
+            } catch (IOException ex) {
+                System.out.println("Unable to Take Screenshot of iOS Simulator");
+            }
+
+        }
+
+    }
+
+    // App Variant Setup Methods
+    private void setAndroidAppVariant(String appVariant) {
+        // Set App Android Variants Value (Optional)
+    }
+
+    private void setIOSdAppVariant(String appVariant) {
+        // Set App iOS Variants Value (Optional)
+    }
+
+    // Access Project-wide Properties
+    static String getValue(String key) {
+
+        try {
+            properties = new Properties();
+            properties.load(new FileInputStream("automation.properties"));
+        } catch (IOException ex ) {
+            System.out.println(ex.getMessage());
+        }
+
+        return properties.getProperty(key);
+    }
+
+}
---
+++ AutomationFoundation/src/test/java/com/automation/foundation/utilityClasses/MobileUtility.java
@@ -0,0 +1,218 @@
+package com.automation.foundation.utilityClasses;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.automation.foundation.utilityClasses.MobileStarter.getValue;
+
+public class MobileUtility {
+
+    // App Platform Drivers
+    private static AndroidDriver<MobileElement> androidDriver;
+    private static IOSDriver<MobileElement> iosDriver;
+
+    // Basic Required Variables
+    private static WebDriverWait wait;
+    private static TouchAction touch;
+
+    // App Platform Verification
+    private static boolean testingAndroid;
+    private static boolean testingIOS;
+
+    // Appium Driver Methods
+    static void startIOSApp() {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, getValue("ios.device")); // iPhone Simulator Being Used
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, getValue("ios.version")); // iOS Version Being Used in Simulator
+        caps.setCapability(MobileCapabilityType.APP, getValue("ios.app"));
+        caps.setCapability("autoAcceptAlerts", true);
+
+        try {
+            iosDriver = new IOSDriver<MobileElement>(new URL(getValue("appium.address")), caps);
+            wait = new WebDriverWait(iosDriver, 60);
+            touch = new TouchAction(iosDriver);
+        } catch (MalformedURLException ex) {
+            iosDriver.resetApp();
+        }
+
+    }
+
+    static void startAndroidApp() {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability("deviceName", getValue("android.device.name")); // Emulator Name
+        caps.setCapability("udid", getValue("android.device.udid")); // Emulator UDID
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", getValue("android.device.version")); // Android Version
+        caps.setCapability("appPackage", getValue("android.apk.package"));
+        caps.setCapability("appActivity", getValue("android.apk.activity"));
+        caps.setCapability("app", getValue("android.apk"));
+
+        try {
+            androidDriver = new AndroidDriver<MobileElement>(new URL(getValue("appium.address")), caps);
+            wait = new WebDriverWait(androidDriver, 60);
+            touch = new TouchAction(androidDriver);
+        } catch (MalformedURLException ex) {
+            androidDriver.resetApp();
+        }
+
+    }
+
+    // Mobile Utility Methods
+    public static void tap(WebElement where) {
+
+        touch.tap(PointOption.point(where.getLocation().getX(), where.getLocation().getY()))
+                .perform();
+    }
+
+    public static void swipe(WebElement from, WebElement to) {
+
+        touch.press(PointOption.point(from.getLocation().getX(), from.getLocation().getY()))
+                .moveTo(PointOption.point(to.getLocation().getX(), to.getLocation().getY()))
+                .release()
+                .perform();
+    }
+
+    public static void scroll(String directionOrElement) {
+
+        HashMap<String, String> scrollSettings = new HashMap<>();
+        scrollSettings.put("direction", directionOrElement.toLowerCase());
+
+        if (testingAndroid) {
+            androidDriver.findElementByAndroidUIAutomator("new UiScrollable("
+                    + "new UiSelector().scrollable(true)).scrollIntoView("
+                    + "new UiSelector().textContains(\"" + directionOrElement + "\"));"); // Text of the Element to Scroll To
+        }
+
+        if (testingIOS) {
+            iosDriver.executeScript("mobile: scroll", scrollSettings); // The "direction" can be up / down / right / left
+        }
+
+    }
+
+    public static WebElement findElementByClassAndText(String elementClass, String elementText) {
+
+        List<MobileElement> elements = androidDriver.findElementsByClassName(elementClass);
+        WebElement foundElement = null;
+
+        for (MobileElement element : elements) {
+            if (element.getAttribute("text").contains(elementText)) {
+                foundElement = element;
+                break;
+            }
+        }
+
+        return foundElement;
+    }
+
+    public static void waitSeconds() {
+
+        if (testingAndroid) {
+            androidDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+
+        if (testingIOS) {
+            iosDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+
+    }
+
+    public static void checkKeyboard() {
+
+        if (testingAndroid && androidDriver.isKeyboardShown()) {
+            androidDriver.hideKeyboard();
+        }
+
+        if (testingIOS && iosDriver.isKeyboardShown()) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeButton[@name=\"Done\"]\n"))).click();
+        }
+
+    }
+
+    public static String randomNumber() {
+
+        int randomNumber = (int) (Math.random() * 999999);
+        return Integer.toString(randomNumber);
+    }
+
+    public static void clickDeleteButton() {
+
+        if (testingAndroid) {
+            androidDriver.pressKey(new KeyEvent().withKey(AndroidKey.DEL));
+        }
+
+        if (testingIOS) {
+            iosDriver.getKeyboard().pressKey(Keys.DELETE);
+        }
+
+    }
+
+    public static void touchFingerprintSensor() {
+
+        if (testingAndroid) {
+            androidDriver.fingerPrint(2);
+        }
+
+        if (testingIOS) {
+            iosDriver.performTouchID(true);
+        }
+
+    }
+
+    // Mobile Helper Getters
+    public static AndroidDriver<MobileElement> getAndroidDriver() {
+        return androidDriver;
+    }
+
+    public static IOSDriver<MobileElement> getIosDriver() {
+        return iosDriver;
+    }
+
+    public static WebDriverWait getWait() {
+        return wait;
+    }
+
+    public static boolean isTestingAndroid() {
+        return testingAndroid;
+    }
+
+    public static boolean isTestingIOS() {
+        return testingIOS;
+    }
+
+    private static TouchAction getTouch() {
+        return touch;
+    }
+
+    // Mobile Setters
+    public static void setTestingAndroid(boolean testingAndroid) {
+        MobileUtility.testingAndroid = testingAndroid;
+    }
+
+    public static void setTestingIOS(boolean testingIOS) {
+        MobileUtility.testingIOS = testingIOS;
+    }
+
+}
