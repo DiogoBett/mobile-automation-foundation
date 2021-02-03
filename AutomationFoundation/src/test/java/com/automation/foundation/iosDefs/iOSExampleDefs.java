b6796fcc542d58d7a5d5538c56c218230093835b
Diogo Bettencourt <diogobettencourt97@gmail.com>
2021-02-03 17:31:51 -0100

    Added Examples in Def Files

--- AutomationFoundation/src/test/java/com/automation/foundation/iosDefs/iOSExampleDefs.java
+++ AutomationFoundation/src/test/java/com/automation/foundation/iosDefs/iOSExampleDefs.java
@@ -1,24 +1,45 @@
 package com.automation.foundation.iosDefs;
 
+import com.automation.foundation.utilityClasses.MobileUtility;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class iOSExampleDefs {
+
+    // Basic Required Variables
+    private IOSDriver<MobileElement> driver = MobileUtility.getIosDriver();
 
     @Given("^I do something in the App - iOS$")
     public void something1() {
         // Cucumber Given Method Implementation
-    }
+
+        // Example Below:
+        WebElement goToFormButton = driver.findElement(By.id("[Element ID]"));
+        goToFormButton.click(); // Clicks the Selected Element
+        }
 
     @When("^I do this in the App - iOS$")
     public void something2() {
         // Cucumber When Method Implementation
+
+        // Example Below:
+        WebElement textFormField = driver.findElement(By.xpath("[Element XPath]"));
+        textFormField.clear(); // Clears the Input Field
+        textFormField.sendKeys("Example"); // Write in the Input Field
     }
 
     @Then("^This should happen - iOS$")
     public void something3() {
         // Cucumber Then Method Implementation
+
+        // Example Below:
+        WebElement submitFormButton = driver.findElement(By.name("[Element Name]"));
+        submitFormButton.click(); // Clicks the Selected Element
     }
 
 }
